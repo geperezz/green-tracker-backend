1fa753555812a082d4510bce1de8b9bc9a49fb3c
geperezz <gph393@gmail.com>
2024-05-29 19:23:55 -0400

    Create the evidence feedback table

---
+++ src/evidence-feedback/evidence-feedback.table.ts
@@ -0,0 +1,49 @@
+import {
+  foreignKey,
+  pgTable,
+  primaryKey,
+  serial,
+  text,
+  uuid,
+} from 'drizzle-orm/pg-core';
+
+import { adminsTable } from 'src/admins/admins.table';
+import { evidenceTable } from 'src/evidence/evidence.table';
+
+export const evidenceFeedbackTable = pgTable(
+  'evidence_feedback',
+  {
+    activityId: uuid('activity_id').notNull(),
+    evidenceNumber: serial('evidence_number').notNull(),
+    adminId: uuid('admin_id')
+      .references(() => adminsTable.id, {
+        onUpdate: 'cascade',
+        onDelete: 'cascade',
+      })
+      .notNull(),
+    feedback: text('feedback').notNull(),
+  },
+  (evidenceFeedbackTable) => {
+    return {
+      primaryKey: primaryKey({
+        columns: [
+          evidenceFeedbackTable.activityId,
+          evidenceFeedbackTable.evidenceNumber,
+          evidenceFeedbackTable.adminId,
+        ],
+      }),
+      evidenceForeignKey: foreignKey({
+        columns: [
+          evidenceFeedbackTable.activityId,
+          evidenceFeedbackTable.evidenceNumber,
+        ],
+        foreignColumns: [
+          evidenceTable.activityId,
+          evidenceTable.evidenceNumber,
+        ],
+      })
+        .onUpdate('cascade')
+        .onDelete('cascade'),
+    };
+  },
+);
