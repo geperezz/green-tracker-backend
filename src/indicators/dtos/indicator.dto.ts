2d0e7716071a2c11c338a8ef8db63068cd6d1643
geperezz <gph393@gmail.com>
2024-06-23 18:29:41 -0400

    Use the `categoryDtoSchema` without extending it

--- src/indicators/dtos/indicator.dto.ts
+++ src/indicators/dtos/indicator.dto.ts
@@ -3,14 +3,9 @@ import { z } from 'nestjs-zod/z';
 
 import { indicatorSchema } from '../schemas/indicator.schema';
 import { categoryDtoSchema } from 'src/categories/dtos/category.dto';
-import { criterionDtoSchema } from 'src/criteria/dtos/criterion.dto';
 
 export const indicatorDtoSchema = indicatorSchema.extend({
-  categories: z.array(
-    categoryDtoSchema.extend({
-      criteria: z.array(criterionDtoSchema.omit({ indicatorIndex: true })),
-    }),
-  ),
+  categories: z.array(categoryDtoSchema),
 });
 
 export class IndicatorDto extends createZodDto(indicatorDtoSchema) {}
